6fcee9f50237566d66c5eda5b83684f52e19d614
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-12-30 19:48:38 +0530

    Added Q2

--- JavaScript/Objects/Output Based Interview Questions.js
+++ JavaScript/Objects/Output Based Interview Questions.js
@@ -16,3 +16,31 @@ console.log(obj);
 // Note: two properties with same name `a` are defined
 
 // ________________________________________________________________
+
+// Question 2:
+// Create a function multiplyByTwo(obj) that multiplies
+// all numeric property values of nums by 2
+
+let nums = {
+  a: 100,
+  b: 200,
+  title: "My nums",
+};
+
+// Ans:
+const multipleByTwo = (obj) => {
+  for (key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] *= 2;
+    }
+  }
+};
+
+multipleByTwo(nums);
+
+console.log(nums);
+
+// OUTPUT:
+// { a: 200, b: 400, title: 'My nums' }
+
+//________________________________________________________________
