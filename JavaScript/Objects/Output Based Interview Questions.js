a545fccdd743c21d9538319a9adaf4f625fb236b
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-12-30 19:48:19 +0530

    Added Q1

---
+++ JavaScript/Objects/Output Based Interview Questions.js
@@ -0,0 +1,18 @@
+// Output Based Interview Questions
+// REF: RSC $ Course
+
+// Question 1: Guess the Output
+
+const obj = {
+  a: "one",
+  b: "two",
+  a: "three",
+};
+
+console.log(obj);
+
+// Ans: { a: 'three', b: 'two' }
+
+// Note: two properties with same name `a` are defined
+
+// ________________________________________________________________
