b3bf46c5ed450b2b169023d8b5fa888dbef50702
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-01-28 11:24:20 +0530

    Added Profile form changes

--- React/Machine Coding Round/tab-multipage-config-form/src/components/Profile.jsx
+++ React/Machine Coding Round/tab-multipage-config-form/src/components/Profile.jsx
@@ -1,7 +1,46 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({ data, setData }) => {
+  const { name, age, email } = data;
+
+  const handleChange = (e) => {
+    // console.log(e.target.name, data);
+    setData((prevData) => {
+      return { ...prevData, [`${e.target.name}`]: e.target.value };
+    });
+  };
+
+  console.log("first", data);
+  return (
+    <div>
+      <h2>Profile</h2>
+
+      <label htmlFor="name">Name</label>
+      <input
+        name="name"
+        type="text"
+        value={name}
+        onChange={(e) => handleChange(e)}
+      />
+
+      <label htmlFor="age">Age</label>
+      <input
+        name="age"
+        type="number"
+        value={age}
+        onChange={(e) => handleChange(e)}
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        name="email"
+        type="email"
+        value={email}
+        onChange={(e) => handleChange(e)}
+      />
+    </div>
+  );
 };
 
 export default Profile;
