2c6f5167fda4c0758ce38f21e31d25f14f5a7535
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-02-04 12:13:49 +0530

    Added Settings code

--- React/Machine Coding Round/tab-multipage-config-form/src/components/Settings.jsx
+++ React/Machine Coding Round/tab-multipage-config-form/src/components/Settings.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-
+/* eslint-disable react/prop-types */
 const Settings = ({ data, setData }) => {
-  console.log("From Settings", { data, setData });
-  return <div>Settings</div>;
+  const { darkTheme } = data;
+
+  const handleClick = (e) => {
+    setData((prevData) => {
+      return { ...prevData, darkTheme: e.target.name };
+    });
+  };
+
+  return (
+    <div>
+      <h2>Settings</h2>
+      <div>
+        <input
+          type="radio"
+          name="dark"
+          checked={darkTheme === "dark"}
+          onChange={(e) => handleClick(e)}
+        />
+        <label htmlFor="dark">Dark</label>
+
+        <input
+          type="radio"
+          name="light"
+          checked={darkTheme === "light"}
+          onChange={handleClick}
+        />
+        <label htmlFor="light">Light</label>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
