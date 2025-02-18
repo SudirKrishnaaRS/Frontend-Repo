07ca22e84ec383af2afcd162294e5c0149aa9c68
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-02-18 17:07:16 +0530

    Added Profile Tab form validations

--- React/Machine Coding Round/tab-multipage-config-form/src/components/Profile.jsx
+++ React/Machine Coding Round/tab-multipage-config-form/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-const Profile = ({ data, setData }) => {
+const Profile = ({ data, setData, errors }) => {
   const { name, age, email } = data;
 
   const handleChange = (e) => {
@@ -10,37 +10,47 @@ const Profile = ({ data, setData }) => {
     });
   };
 
-  console.log("first", data);
+  // console.log("first", data);
+
   return (
-    <div>
+    <div className="tab-content-container">
       <h2>Profile</h2>
 
-      <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        name="name"
-        type="text"
-        value={name}
-        onChange={(e) => handleChange(e)}
-      />
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={name}
+          onChange={(e) => handleChange(e)}
+        />
+        {errors?.name && <span className="error">{errors.name}</span>}
+      </div>
 
-      <label htmlFor="age">Age</label>
-      <input
-        id="age"
-        name="age"
-        type="number"
-        value={age}
-        onChange={(e) => handleChange(e)}
-      />
+      <div>
+        <label htmlFor="age">Age</label>
+        <input
+          id="age"
+          name="age"
+          type="number"
+          value={age}
+          onChange={(e) => handleChange(e)}
+        />
+        {errors?.age && <span className="error">{errors.age}</span>}
+      </div>
 
-      <label htmlFor="email">email</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        value={email}
-        onChange={(e) => handleChange(e)}
-      />
+      <div>
+        <label htmlFor="email">email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => handleChange(e)}
+        />
+        {errors?.email && <span className="error">{errors.email}</span>}
+      </div>
     </div>
   );
 };
