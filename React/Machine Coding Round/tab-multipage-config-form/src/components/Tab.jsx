0e8a094b5a93965add2fabd1b89f796bd3ffa800
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-02-18 17:06:32 +0530

    Added logic for Form validation

--- React/Machine Coding Round/tab-multipage-config-form/src/components/Tab.jsx
+++ React/Machine Coding Round/tab-multipage-config-form/src/components/Tab.jsx
@@ -3,21 +3,6 @@ import Profile from "./Profile";
 import Interest from "./Interest";
 import Settings from "./Settings";
 
-const Tabs = [
-  {
-    name: "Profile",
-    component: Profile,
-  },
-  {
-    name: "Interests",
-    component: Interest,
-  },
-  {
-    name: "Settings",
-    component: Settings,
-  },
-];
-
 const Tab = () => {
   const [activeTab, setActiveTab] = useState(0);
   const [data, setData] = useState({
@@ -27,19 +12,75 @@ const Tab = () => {
     interests: ["coding", "javascript"],
     darkTheme: "dark",
   });
+
+  const [errors, setErrors] = useState({});
+
+  const Tabs = [
+    {
+      name: "Profile",
+      component: Profile,
+      validate: () => {
+        const err = {};
+
+        if (!data.name || data.name.length < 2) {
+          err.name = "Name is not valid";
+        }
+
+        if (!data.age || data.age < 18) {
+          err.age = "Age is not valid";
+        }
+
+        if (!data.email || data.email.length < 2) {
+          err.email = "Email is not valid";
+        }
+
+        setErrors(err);
+        return err.name || err.age || err.email;
+      },
+    },
+    {
+      name: "Interests",
+      component: Interest,
+      validate: () => {
+        const err = {};
+
+        if (!data.interests || data.interests.length < 1) {
+          err.interests = "Select atleast one interest";
+        }
+
+        setErrors(err);
+        return err.interests;
+      },
+    },
+    {
+      name: "Settings",
+      component: Settings,
+      validate: () => {
+        // This tab has NO Form Validations so we just return TRUE
+        return true;
+      },
+    },
+  ];
+
   const ActiveTabComponent = Tabs[activeTab].component;
 
   //   console.log(activeTab, ActiveTabComponent);
 
   const handlePrevious = () => {
-    if (activeTab > 0 && activeTab <= Tabs.length - 1) {
-      setActiveTab((currTab) => currTab - 1);
+    // Enable if there's no Errors
+    if (!Tabs[activeTab].validate()) {
+      if (activeTab > 0 && activeTab <= Tabs.length - 1) {
+        setActiveTab((currTab) => currTab - 1);
+      }
     }
   };
 
   const handleNext = () => {
-    if (activeTab < Tabs.length - 1) {
-      setActiveTab((currTab) => currTab + 1);
+    // Enable if there's no Errors
+    if (!Tabs[activeTab].validate()) {
+      if (activeTab < Tabs.length - 1) {
+        setActiveTab((currTab) => currTab + 1);
+      }
     }
   };
 
@@ -62,7 +103,7 @@ const Tab = () => {
       })}
 
       <div>
-        <ActiveTabComponent data={data} setData={setData} />
+        <ActiveTabComponent data={data} setData={setData} errors={errors} />
       </div>
 
       <br />
