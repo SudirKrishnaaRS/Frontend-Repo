8537a48219d9b9a20cd3527ceb1a006071c9b3c8
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-04-09 11:51:21 +0530

    Added basic jsx for OTP input

--- React/Machine Coding Round/otp-input/src/App.jsx
+++ React/Machine Coding Round/otp-input/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
+const OTP_INPUT_SIZE = 5;
+
 function App() {
+  const [inputArr, setInputArr] = useState(new Array(OTP_INPUT_SIZE).fill(""));
+
   return (
     <>
       <h2>OTP Validation</h2>
+      {inputArr.map((input, index) => {
+        return (
+          <input
+            type="text"
+            className="otp-input"
+            value={inputArr[index]}
+            key={index}
+          />
+        );
+      })}
     </>
   );
 }
