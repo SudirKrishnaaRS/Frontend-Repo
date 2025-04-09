d3354ccffef50a91634ac3fd1cb1828198e8b288
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-04-09 12:15:27 +0530

    Added Ref and focus input changes

--- React/Machine Coding Round/otp-input/src/App.jsx
+++ React/Machine Coding Round/otp-input/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const OTP_INPUT_SIZE = 5;
 
 function App() {
   const [inputArr, setInputArr] = useState(new Array(OTP_INPUT_SIZE).fill(""));
+
+  const refArr = useRef([]);
+
+  useEffect(() => {
+    refArr.current[0]?.focus();
+  }, []);
 
   const handleChange = (val, index) => {
     // TRIM: Remove the leading and trailing white space
@@ -18,6 +24,9 @@ function App() {
     const newArr = [...inputArr];
     newArr[index] = updatedVal.slice(-1); // SLICE: to accept the latest typed value
     setInputArr(newArr);
+
+    // Focus on the next input box
+    updatedVal && refArr.current[index + 1]?.focus();
   };
 
   return (
@@ -28,8 +37,11 @@ function App() {
           <input
             type="text"
             className="otp-input"
-            value={inputArr[index]}
             key={index}
+            ref={(input) => {
+              return (refArr.current[index] = input);
+            }}
+            value={inputArr[index]}
             onChange={(e) => handleChange(e.target.value, index)}
           />
         );
