7b98ced5c517e60bd110f367e91090f14f18f37b
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-04-10 13:31:29 +0530

    Added Progress Bar Simple

--- React/Machine Coding Round/Progress-bar/src/App.jsx
+++ React/Machine Coding Round/Progress-bar/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import ProgressBar from "./components/ProgressBar/ProgressBar";
 
 function App() {
+  const bars = [5, 10, 25, 50, 75, 90, 100];
+
   return (
     <>
       <h2>Progress Bar</h2>
+      {bars.map((bar, index) => {
+        return <ProgressBar key={index} progress={bar} />;
+      })}
     </>
   );
 }
