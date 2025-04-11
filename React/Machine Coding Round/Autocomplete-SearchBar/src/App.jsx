23031f6b032f29699c95f2da572134a016d33ab8
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-04-11 13:39:25 +0530

    Added Basic SearchResults JSX

--- React/Machine Coding Round/Autocomplete-SearchBar/src/App.jsx
+++ React/Machine Coding Round/Autocomplete-SearchBar/src/App.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+// API: 'https://dummyjson.com/recipes/search?q=Margherita'
+
 function App() {
+  const [searchVal, setSearchVal] = useState("");
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://dummyjson.com/recipes/search?q=${searchVal}`
+    );
+    const result = await response.json();
+    setData(result);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [searchVal]);
+
   return (
     <>
       <h2>Autocomplete Search Bar</h2>
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="search here..."
+        value={searchVal}
+        onChange={(e) => setSearchVal(e.target.value)}
+      />
+      <div className="search-results-container">
+        {data?.recipes?.map((ele) => {
+          return (
+            <span key={ele.id} className="search-result">
+              {ele.name}
+            </span>
+          );
+        })}
+      </div>
     </>
   );
 }
