1215b6bf7542a625e605b7609fe45f33270a6766
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-04-14 12:45:01 +0530

    Added Comments

--- Coding Questions/Javascript/Todo List in JS/script.js
+++ Coding Questions/Javascript/Todo List in JS/script.js
@@ -5,18 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.querySelector(".todo-list");
 
   todoForm.addEventListener("submit", () => {
-    event.preventDefault();
+    event.preventDefault(); // prevent the default behaviour of reload when submission of form
 
-    const todoInputValue = todoInput.value.trim();
+    const todoInputValue = todoInput.value.trim(); // trim the input for leading & trailing space
     if (todoInputValue !== "") {
       addItem(todoInputValue);
 
-      todoInput.value = "";
+      todoInput.value = ""; // once the item is added to list, erase the input value
     } else {
-      alert("Enter a valid input");
+      alert("Enter a valid input"); // If the input is empty then ALERT the user
     }
   });
 
+  //ADD an item to todo list
   const addItem = (todoInputValue) => {
     const listItem = document.createElement("li");
     const editButton = document.createElement("button");
