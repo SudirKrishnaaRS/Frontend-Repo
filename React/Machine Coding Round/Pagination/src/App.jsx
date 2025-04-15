a8b247e2f17b3c83843fa0ebc77f3db318724ad2
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-04-15 12:51:50 +0530

    Added Pagination

--- React/Machine Coding Round/Pagination/src/App.jsx
+++ React/Machine Coding Round/Pagination/src/App.jsx
@@ -2,11 +2,14 @@ import { useEffect } from "react";
 import "./App.css";
 import { useState } from "react";
 import ProductCard from "./components/ProductCard/ProductCard";
+import { PAGE_SIZE } from "./utils/constants";
 
 //API Endpoint: 'https://dummyjson.com/products?limit=500'
 
 function App() {
   const [data, setData] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(0);
 
   const fetchData = async () => {
     const response = await fetch("https://dummyjson.com/products?limit=500");
@@ -18,6 +21,15 @@ function App() {
     fetchData();
   }, []);
 
+  const total = data.length;
+  const pageNumber = Math.ceil(total / PAGE_SIZE);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  const handlePageChange = (num) => {
+    setCurrentPage(num);
+  };
+
   if (data.length <= 0) {
     return <div>No products found</div>;
   }
@@ -25,8 +37,17 @@ function App() {
   return (
     <>
       <h1>Pagination</h1>
+      {/* PAGINATION */}
+      {[...Array(pageNumber).keys()].map((num, index) => {
+        return (
+          <button key={index} onClick={() => handlePageChange(num)}>
+            {num + 1}
+          </button>
+        );
+      })}
+
       <div className="product-container">
-        {data?.map((ele) => {
+        {data?.slice(start, end).map((ele) => {
           return (
             <ProductCard key={ele.id} title={ele.title} image={ele.thumbnail} />
           );
